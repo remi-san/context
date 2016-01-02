50659cb64f441205e4f4f7f1df35a2e8b1dfffc9
remi-san <remi.san@gmail.com>
2016-01-02 22:25:03 +0000

    context is noaw an interface

--- src/Context.php
+++ src/Context.php
@@ -1,28 +1,10 @@
 <?php
 namespace RemiSan\Command;
 
-class Context
+interface Context
 {
-    /**
-     * @var mixed
-     */
-    private $value;
-
-    /**
-     * Constructor
-     *
-     * @param mixed $value
-     */
-    public function __construct($value)
-    {
-        $this->value = $value;
-    }
-
     /**
      * @return mixed
      */
-    public function getValue()
-    {
-        return $this->value;
-    }
+    public function getValue();
 }
